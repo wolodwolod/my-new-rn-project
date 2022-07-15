a303cb89ce8ea3977195a594c55de12d0bc42a5b
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2022-07-16 00:25:28 +0300

    Update CommentsScreen.js

--- screens/nestedScreens/CommentsScreen.js
+++ screens/nestedScreens/CommentsScreen.js
@@ -1,37 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  SafeAreaView,
+  FlatList,
 } from "react-native";
 import { useSelector } from "react-redux";
-import { collection, addDoc, doc } from "firebase/firestore";
+import { collection, addDoc, doc, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/config";
 
 const CommentsScreen = ({ route }) => {
   const { postId } = route.params;
-  console.log("postId", postId);
+  // console.log("postId", postId);
 
   const [comment, setComment] = useState("");
+  const [allComments, setAllComments] = useState([]);
   const { login } = useSelector((state) => state.auth);
 
+  const postRef = doc(db, "posts", postId);
+
+  useEffect(() => {
+    getAllComments();
+  }, []);
+
   const createComment = async () => {
-    const postRef = doc(db, "posts", postId);
     await addDoc(collection(postRef, "comments"), {
       comment,
       login,
     });
   };
 
+  const getAllComments = async () => {
+    const querySnapshot = await getDocs(collection(postRef, "comments"));
+    const allPostComments = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setAllComments(allPostComments);
+    console.log("allComments", allComments);
+  };
+
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={styles.commentContainer}>
+              <Text>{item.login}</Text>
+              <Text>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} onChangeText={setComment} />
       </View>
       <TouchableOpacity onPress={createComment} style={styles.sendBtn}>
-        <Text style={styles.sendLabel}>add post</Text>
+        <Text style={styles.sendLabel}>add comment</Text>
       </TouchableOpacity>
     </View>
   );
@@ -40,7 +70,13 @@ const CommentsScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
+  },
+  commentContainer: {
+    borderWidth: 1,
+    borderColor: "#20b2aa",
+    marginHorizontal: 10,
+    padding: 10,
+    marginBottom: 10,
   },
   sendBtn: {
     marginHorizontal: 30,
